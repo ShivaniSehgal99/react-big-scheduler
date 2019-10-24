ba932725a75eb77b7829567e292d90ec0b14399d
Shivani Sehgal <shivani.sehgal@daffodilsw.com>
2019-10-24 15:02:45 +0530

    updated resourceComponent prop

--- src/ResourceView.js
+++ src/ResourceView.js
@@ -18,7 +18,7 @@ class ResourceView extends Component {
 
     render() {
 
-        const {schedulerData, contentScrollbarHeight, slotClickedFunc, slotItemTemplateResolver, toggleExpandFunc, handleClickResource, ResourceStatus} = this.props;
+        const {schedulerData, contentScrollbarHeight, slotClickedFunc, slotItemTemplateResolver, toggleExpandFunc, handleClickResource, resourceStatus} = this.props;
         const {renderData} = schedulerData;
 
         let width = schedulerData.getResourceTableWidth() - 2;
@@ -77,7 +77,8 @@ class ResourceView extends Component {
                         <div className={item.parentClass} onClick={() => handleClickResource(item)}>
                             <div className={item.childClass}></div>
                         </div>
-                        <div className='status-list'>{ResourceStatus}</div>
+                        {item.slotId === resourceStatus.slotId
+                            && <div className='status-list'>{resourceStatus.resourceStatusComponent}</div>}
                     </td>
                 </tr>
             );
